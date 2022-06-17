9a1308223417da70bb7b774ca1002971fa086dc7
Darshan-V <mr.darshanv@gmail.com>
2022-06-17 12:47:29 +0530

    regular expression-learning

--- regex.js
+++ regex.js
@@ -1,5 +1,26 @@
 // Fill in the regular expressions
 
+///abc/	A sequence of characters
+///[abc]/	Any character from a set of characters
+///[^abc]/	Any character not in a set of characters
+///[0-9]/	Any character in a range of characters
+///x+/	One or more occurrences of the pattern x
+///x+?/	One or more occurrences, nongreedy
+///x*/	Zero or more occurrences
+/*
+/x?/	Zero or one occurrence
+/x{2,4}/	Two to four occurrences
+/(abc)/	A group
+/a|b|c/	Any one of several patterns
+/\d/	Any digit character
+/\w/	An alphanumeric character (“word character”)
+/\s/	Any whitespace character
+/./	Any character except newlines
+/\b/	A word boundary
+/^/	Start of input
+/$/	End of input
+*/
+
 verify(/ca[rt]/,
   ['my car', 'bad cats'],
   ['camper', 'high art'])
@@ -28,7 +49,7 @@ verify(/\b[^\We]+\b/i,
   ['red platypus', 'wobbling nest'],
   ['earth bed', 'learning ape', 'BEET'])
 
-function verify (regexp, yes, no) {
+function verify(regexp, yes, no) {
   // Ignore unfinished exercises
   if (regexp.source === '...') return
   for (const str of yes) {
